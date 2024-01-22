37e395aef0ed62660832df2dc18ae9fae8fb61bd
kdmin0706 <gxxk76@gmail.com>
2024-01-22 15:38:38 +0900

    feat: initial commit

---
+++ src/main/java/com/halfgallon/withcon/WithconApplication.java
@@ -0,0 +1,13 @@
+package com.halfgallon.withcon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WithconApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WithconApplication.class, args);
+	}
+
+}
