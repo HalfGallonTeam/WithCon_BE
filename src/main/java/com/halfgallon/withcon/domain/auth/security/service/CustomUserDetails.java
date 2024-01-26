94120be2ccf6f35aa291d702a4745d9cc1ecb717
cchoijjinyoung <cgy7127@naver.com>
2024-01-27 02:47:16 +0900

    feat: 로그인 인증을 위한 CustomUserDetails, CustomUserDetailsService 구현
    CustomUserDetails: 회원의 인증 정보와 권한 정보를 담는 클래스
    CustomUserDetailsService: 회원의 인증 정보를 조회하고 CustomUserDetails 객체를 생성하는 서비스

---
+++ src/main/java/com/halfgallon/withcon/domain/auth/security/service/CustomUserDetails.java
@@ -0,0 +1,62 @@
+package com.halfgallon.withcon.domain.auth.security.service;
+
+import com.halfgallon.withcon.domain.member.entity.Member;
+import java.util.Collection;
+import java.util.List;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class CustomUserDetails implements UserDetails {
+
+  private final Long id;
+
+  private final String username;
+
+  private final String password;
+
+  public CustomUserDetails(Member member) {
+    this.id = member.getId();
+    this.username = member.getUsername();
+    this.password = member.getPassword();
+  }
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  @Override
+  public String getUsername() {
+    return username;
+  }
+
+  @Override
+  public String getPassword() {
+    return password;
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return true;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return true;
+  }
+}
---
+++ src/main/java/com/halfgallon/withcon/domain/auth/security/service/CustomUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.halfgallon.withcon.domain.auth.security.service;
+
+import com.halfgallon.withcon.domain.member.entity.Member;
+import com.halfgallon.withcon.domain.member.repository.MemberRepository;
+import com.halfgallon.withcon.global.exception.CustomException;
+import com.halfgallon.withcon.global.exception.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+  private final MemberRepository memberRepository;
+
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    Member findMember = memberRepository.findByUsername(username)
+        .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
+
+    return new CustomUserDetails(findMember);
+  }
+}
