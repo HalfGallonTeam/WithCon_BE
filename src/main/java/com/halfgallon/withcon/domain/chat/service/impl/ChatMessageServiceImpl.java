b449e1ab79de19913f01d9efee0d90d3e626f4fc
kdmin0706 <gxxk76@gmail.com>
2024-01-26 13:57:38 +0900

    feat: 메시지 송신 시에 Member 예외 처리 추가

--- src/main/java/com/halfgallon/withcon/domain/chat/service/impl/ChatMessageServiceImpl.java
+++ src/main/java/com/halfgallon/withcon/domain/chat/service/impl/ChatMessageServiceImpl.java
@@ -1,9 +1,14 @@
 package com.halfgallon.withcon.domain.chat.service.impl;
 
+import static com.halfgallon.withcon.global.exception.ErrorCode.USER_NOT_FOUND;
+
 import com.halfgallon.withcon.domain.chat.constant.MessageType;
 import com.halfgallon.withcon.domain.chat.dto.ChatMessageRequest;
 import com.halfgallon.withcon.domain.chat.dto.ChatMessageResponse;
 import com.halfgallon.withcon.domain.chat.service.ChatMessageService;
+import com.halfgallon.withcon.domain.member.entity.Member;
+import com.halfgallon.withcon.domain.member.repository.MemberRepository;
+import com.halfgallon.withcon.global.exception.CustomException;
 import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,6 +16,8 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class ChatMessageServiceImpl implements ChatMessageService {
+
+  private final MemberRepository memberRepository;
 
   @Override
   public ChatMessageResponse chatMessage(ChatMessageRequest request, Long roomId) {
@@ -26,7 +33,10 @@ public class ChatMessageServiceImpl implements ChatMessageService {
   @Override
   public ChatMessageResponse enterMessage(ChatMessageRequest request, Long roomId) {
 
-    String message = "님이 입장하였습니다.";
+    Member member = memberRepository.findById(request.getMemberId())
+        .orElseThrow(() -> new CustomException(USER_NOT_FOUND));
+
+    String message = member.getNickname() + "님이 입장하였습니다.";
 
     return ChatMessageResponse.builder()
         .chatRoomId(roomId)
@@ -39,7 +49,11 @@ public class ChatMessageServiceImpl implements ChatMessageService {
 
   @Override
   public ChatMessageResponse exitMessage(ChatMessageRequest request, Long roomId) {
-    String message = "님이 퇴장하였습니다.";
+
+    Member member = memberRepository.findById(request.getMemberId())
+        .orElseThrow(() -> new CustomException(USER_NOT_FOUND));
+
+    String message = member.getNickname() + "님이 퇴장하였습니다.";
 
     return ChatMessageResponse.builder()
         .chatRoomId(roomId)
