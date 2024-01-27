dfb4cda6af07ce55ceffa9ccbc91b7e8532dc5f2
kdmin0706 <gxxk76@gmail.com>
2024-01-27 17:36:54 +0900

    feat: #17 Querydsl을 이용한 페이지네이션 구성 - JPQL `@Query` 문을 이용한 쿼리문을 `QueryDsl` 로 변경 - PageableExecutionUtils.getPage() 방식을 이용하여 페이징 최적화 진행   - `new PageImpl()`을 이용하면 무조건적으로 count 쿼리 진행 -> 이로 인해 쿼리를 총 2번(select, count) 쿼리를 진행하게 된다.   - `PageableExecutionUtils.getPage()`는 `totalSupplier` 라는 함수형 인터페이스를 사용하여 특정 조건 즉,페이지의 콘텐츠 사이즈가 한 페이지의 사이즈보다 작을 경우 count 쿼리를 진행하지 않는다.   - 이를 바탕으로 성능 최적화를 할 수 있다고 볼 수 있다.

--- src/main/java/com/halfgallon/withcon/domain/chat/repository/impl/CustomChatParticipantRepositoryImpl.java
+++ src/main/java/com/halfgallon/withcon/domain/chat/repository/impl/CustomChatParticipantRepositoryImpl.java
@@ -1,10 +1,17 @@
 package com.halfgallon.withcon.domain.chat.repository.impl;
 
 import static com.halfgallon.withcon.domain.chat.entity.QChatParticipant.chatParticipant;
+import static com.halfgallon.withcon.domain.chat.entity.QChatRoom.chatRoom;
 
+import com.halfgallon.withcon.domain.chat.entity.ChatParticipant;
 import com.halfgallon.withcon.domain.chat.repository.CustomChatParticipantRepository;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.support.PageableExecutionUtils;
 
 @RequiredArgsConstructor
 public class CustomChatParticipantRepositoryImpl implements CustomChatParticipantRepository {
@@ -22,4 +29,21 @@ public class CustomChatParticipantRepositoryImpl implements CustomChatParticipan
     return fetchOne != null;
   }
 
+  @Override
+  public Page<ChatParticipant> findAllMyChattingRoom(Long memberId, Pageable pageable) {
+    List<ChatParticipant> list = jpaQueryFactory.selectFrom(chatParticipant)
+        .innerJoin(chatParticipant.chatRoom, chatRoom)
+        .fetchJoin()
+        .where(chatParticipant.member.id.eq(memberId))
+        .orderBy(chatParticipant.chatRoom.userCount.desc())
+        .offset(pageable.getOffset())
+        .limit(pageable.getPageSize())
+        .fetch();
+
+    JPAQuery<Long> countQuery = jpaQueryFactory.select(chatParticipant.count())
+        .from(chatParticipant)
+        .where(chatParticipant.member.id.eq(memberId));
+
+    return PageableExecutionUtils.getPage(list, pageable, countQuery::fetchOne);
+  }
 }
