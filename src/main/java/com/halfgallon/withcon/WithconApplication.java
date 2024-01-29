e348cdcde69941f19fd09d9bc4df9b52b2ba7087
sosa7753 <sosa7753@naver.com>
2024-01-29 20:56:55 +0900

    chore 설정 변경

--- src/main/java/com/halfgallon/withcon/WithconApplication.java
+++ src/main/java/com/halfgallon/withcon/WithconApplication.java
@@ -2,8 +2,9 @@ package com.halfgallon.withcon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication()
 public class WithconApplication {
 
 	public static void main(String[] args) {
