f7d30cdd61fa944b17dcec04014751755e63ab6f
sosa7753 <sosa7753@naver.com>
2024-01-31 16:55:31 +0900

    feat 피드백 반영

--- src/main/java/com/halfgallon/withcon/domain/notification/controller/NotificationController.java
+++ src/main/java/com/halfgallon/withcon/domain/notification/controller/NotificationController.java
@@ -8,10 +8,8 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
