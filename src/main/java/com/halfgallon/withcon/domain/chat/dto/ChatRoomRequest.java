c452f726f8df48fea709c481aaf64a6b85a5493b
kdmin0706 <gxxk76@gmail.com>
2024-01-31 15:45:37 +0900

    feat: #31 채팅 태그 기능 추가  - 채팅 태그(tag) 객체 생성(Entity, Repository, Dto)  - 채팅룸 <-> 태그 양방향 매핑 진행(1:N)  - `ChatRoomRequest`, `ChatRoomResponse` 시에 `tagList` 추가  - 채팅방 생성(`createChatRoom`) 시에 tag가 `null`이 아닌 경우 태그 정보 저장

--- src/main/java/com/halfgallon/withcon/domain/chat/dto/ChatRoomRequest.java
+++ src/main/java/com/halfgallon/withcon/domain/chat/dto/ChatRoomRequest.java
@@ -1,12 +1,13 @@
 package com.halfgallon.withcon.domain.chat.dto;
 
 import com.halfgallon.withcon.domain.chat.entity.ChatRoom;
+import java.util.List;
 
 public record ChatRoomRequest(
     Long memberId,
-    String name
+    String name,
+    List<String> tags
 ) {
-
   public ChatRoom toEntity() {
     return ChatRoom.builder()
         .name(this.name)
