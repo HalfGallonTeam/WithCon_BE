6ad2d9b90159fa000ca6ead156e3cd2f5a7c9162
kdmin0706 <gxxk76@gmail.com>
2024-02-01 12:43:36 +0900

    feat: #31 채팅 태그 조회 기능 추가  - `TagCountDto` 생성  - `DTO` 반환 형식의 `Querydsl` 구현     - `tag Count` 내림차순 정렬 조회 기능 구현     - 기존 만들었던 엔티티의 경우에는 `count`를 반환하지 않는다. 하지만 HTTP 요청 시에 필요한 내용이었기 때문에 `count`를 구성하는 `DTO`를 생성하여 쿼리문을 반환하도록 기능을 구현했습니다.

---
+++ src/main/java/com/halfgallon/withcon/domain/tag/controller/TagController.java
@@ -0,0 +1,27 @@
+package com.halfgallon.withcon.domain.tag.controller;
+
+import com.halfgallon.withcon.domain.tag.service.TagService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/tag")
+@RequiredArgsConstructor
+public class TagController {
+
+  private final TagService tagService;
+
+
+  /**
+   * 태그 정보 조회(태그 갯수 많은 순으로 정렬)
+   * @return : name(태그 이름), count(태그 생성 갯수)
+   */
+  @GetMapping
+  public ResponseEntity<?> findTagOrderByCount() {
+    return ResponseEntity.ok(tagService.findTagOrderByCount());
+  }
+
+}
