0dd6f75001a2e29c66b25c5f9d48561239fc7322
cchoijjinyoung <cgy7127@naver.com>
2024-02-01 17:00:39 +0900

    feat: Auth Constant 클래스 추가
    공통으로 사용되는 상수들을 관리하기 위해 Constant 클래스를 작성하였습니다.

---
+++ src/main/java/com/halfgallon/withcon/domain/auth/constant/AuthConstant.java
@@ -0,0 +1,9 @@
+package com.halfgallon.withcon.domain.auth.constant;
+
+public class AuthConstant {
+
+  public static final String ACCESS_TOKEN_HEADER_NAME = "Authorization";
+  public static final String REFRESH_TOKEN_COOKIE_NAME = "refresh_token";
+  public static final String ACCESS_TOKEN_PREFIX = "Bearer ";
+
+}
