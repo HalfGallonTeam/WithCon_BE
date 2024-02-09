9652035c05b91693818feebc48a28687163f5d3f
kdmin0706 <gxxk76@gmail.com>
2024-02-10 04:44:23 +0900

    chore: #72 공연 정보 ddl 파일 생성 - performance, performance_detail ddl 파일 생성

---
+++ sql/performance.sql
@@ -0,0 +1,45 @@
+INSERT INTO performance (id, created_at, modified_at, end_date, facility, likes, name, poster, start_date, status)
+VALUES ('1', LOCALTIME, LOCALTIME, '2024-02-24', '달밤엔시어터', 0, '안녕달아', 'https://www.kopis.or.kr/upload/pfmPoster/PF_PF234853_240202_100411.gif', '2024-02-09', 'RUNNING');
+
+INSERT INTO performance (id, created_at, modified_at, end_date, facility, likes, name, poster, start_date, status)
+VALUES ('2', LOCALTIME, LOCALTIME, '2024-02-11', '소극장 혜화당(구. 까망소극장)', 0, '날이면 날마다 낭독해', 'https://www.kopis.or.kr/upload/pfmPoster/PF_PF234735_240131_102237.jpg', '2024-02-09', 'RUNNING');
+
+INSERT INTO performance (id, created_at, modified_at, end_date, facility, likes, name, poster, start_date, status)
+VALUES ('3', LOCALTIME, LOCALTIME, '2024-03-31', '아트하우스', 0, '거꾸로 청개구리', 'https://www.kopis.or.kr/upload/pfmPoster/PF_PF234570_240129_095812.png', '2024-02-03', 'RUNNING');
+
+INSERT INTO performance (id, created_at, modified_at, end_date, facility, likes, name, poster, start_date, status)
+VALUES ('4', LOCALTIME, LOCALTIME, '2024-03-03', '예술의전당 [서울] 자유소극장', 0, '이상한 나라의 아빠', 'https://www.kopis.or.kr/upload/pfmPoster/PF_PF233138_231228_093133.gif', '2024-01-28', 'RUNNING');
+
+INSERT INTO performance (id, created_at, modified_at, end_date, facility, likes, name, poster, start_date, status)
+VALUES ('5', LOCALTIME, LOCALTIME, '2024-02-25', '상상나라극장', 0, '넌 특별하단다', 'https://www.kopis.or.kr/upload/pfmPoster/PF_PF232994_231222_125048.gif', '2024-01-20', 'RUNNING');
+
+INSERT INTO performance (id, created_at, modified_at, end_date, facility, likes, name, poster, start_date, status)
+VALUES ('6', LOCALTIME, LOCALTIME, '2024-03-24', '세종문화회관 세종대극장', 0, '노트르담 드 파리', 'https://www.kopis.or.kr/upload/pfmPoster/PF_PF230511_231122_093753.png', '2024-01-24', 'RUNNING');
+
+INSERT INTO performance (id, created_at, modified_at, end_date, facility, likes, name, poster, start_date, status)
+VALUES ('7', LOCALTIME, LOCALTIME, '2024-02-25', '한전아트센터', 0, '겨울나그네', 'https://www.kopis.or.kr/upload/pfmPoster/PF_PF230087_231116_100306.gif', '2023-12-15', 'RUNNING');
+
+INSERT INTO performance (id, created_at, modified_at, end_date, facility, likes, name, poster, start_date, status)
+VALUES ('8', LOCALTIME, LOCALTIME, '2024-03-10', '드림시어터 [대학로]', 0, '창작단막극축제: 뿔난이들', 'https://www.kopis.or.kr/upload/pfmPoster/PF_PF230087_231116_100306.gif', '2024-02-06', 'RUNNING');
+
+INSERT INTO performance (id, created_at, modified_at, end_date, facility, likes, name, poster, start_date, status)
+VALUES ('9', LOCALTIME, LOCALTIME, '2024-02-24', '하땅세극장', 0, '고래바위에서 기다려', 'https://www.kopis.or.kr/upload/pfmPoster/PF_PF233723_240111_093113.gif', '2024-01-20', 'RUNNING');
+
+INSERT INTO performance (id, created_at, modified_at, end_date, facility, likes, name, poster, start_date, status)
+VALUES ('10', LOCALTIME, LOCALTIME, '2024-02-18', '벨로주 [홍대] (구. 왓에버)', 0, '구원찬 단독공연, HOMESICK', 'https://www.kopis.or.kr/upload/pfmPoster/PF_PF233882_240115_105255.gif', '2024-02-09', 'RUNNING');
+
+INSERT INTO performance (id, created_at, modified_at, end_date, facility, likes, name, poster, start_date, status)
+VALUES ('11', LOCALTIME, LOCALTIME, '2024-02-18', '이화여고 100주년 기념관', 0, 'HYNN(박혜원) 첫 소극장 콘서트, The Cabin', 'https://www.kopis.or.kr/upload/pfmPoster/PF_PF233882_240115_105255.gif', '2024-02-02', 'RUNNING');
+
+INSERT INTO performance (id, created_at, modified_at, end_date, facility, likes, name, poster, start_date, status)
+VALUES ('12', LOCALTIME, LOCALTIME, '2024-02-24', '파닥파닥클럽', 0, '금토재즈클럽 (2월), The Cabin', 'https://www.kopis.or.kr/upload/pfmPoster/PF_PF234350_240123_104211.png', '2024-02-02', 'RUNNING');
+
+INSERT INTO performance (id, created_at, modified_at, end_date, facility, likes, name, poster, start_date, status)
+VALUES ('13', LOCALTIME, LOCALTIME, '2024-02-25', 'SK올림픽핸드볼경기장', 0, '2024 10CM ENCORE CONCERT ‘=10’', 'https://ticketimage.interpark.com/Play/image/large/24/24001528_p.gif', '2024-02-24', 'WAITING');
+
+INSERT INTO performance (id, created_at, modified_at, end_date, facility, likes, name, poster, start_date, status)
+VALUES ('14', LOCALTIME, LOCALTIME, '2024-02-18', '올림픽공원 올림픽홀', 0, '2024 로꼬 콘서트 ‘ALL I NEED’', 'https://ticketimage.interpark.com/Play/image/large/23/23018690_p.gif', '2024-02-17', 'WAITING');
+
+INSERT INTO performance (id, created_at, modified_at, end_date, facility, likes, name, poster, start_date, status)
+VALUES ('15', LOCALTIME, LOCALTIME, '2024-02-17', '블루스퀘어 마스터카드홀', 0, 'LEE YOUNGJI 1st ASIA TOUR ‘THE MAIN CHARACTER’ in SEOUL', 'https://ticketimage.interpark.com/Play/image/large/24/24000313_p.gif', '2024-02-18', 'WAITING');
+
