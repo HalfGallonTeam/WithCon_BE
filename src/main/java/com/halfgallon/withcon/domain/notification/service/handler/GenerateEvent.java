dc83a6b4d027fa04d3d13cfa7f08391d1b752127
sosa7753 <sosa7753@naver.com>
2024-02-11 00:09:35 +0900

    feat Event 발행 -Event 발생시 발행과 함께 알림 저장 및 전송을 수행합니다.

--- src/main/java/com/halfgallon/withcon/domain/notification/service/handler/GenerateEvent.java
+++ src/main/java/com/halfgallon/withcon/domain/notification/service/handler/GenerateEvent.java
@@ -5,7 +5,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 
 @Slf4j
 @Component
--- src/main/java/com/halfgallon/withcon/domain/notification/service/handler/NotificationScheduleHandler.java
+++ src/main/java/com/halfgallon/withcon/domain/notification/service/handler/NotificationScheduleHandler.java
@@ -6,7 +6,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
