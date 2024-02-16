1de130a7f3eead1087d7de8a2066565306764d51
sosa7753 <sosa7753@naver.com>
2024-02-16 15:27:07 +0900

    feat 찜 기능 추가 - 메인페이지 장르별 목록 조회 - 공연 찜 하기 - 공연 찜 취소 하기 - 나의 찜 공연ID 목록 조회 - 나의 찜 공연 목록 조회

--- src/main/java/com/halfgallon/withcon/global/exception/ErrorCode.java
+++ src/main/java/com/halfgallon/withcon/global/exception/ErrorCode.java
@@ -2,7 +2,6 @@ package com.halfgallon.withcon.global.exception;
 
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.CONFLICT;
-import static org.springframework.http.HttpStatus.CONTINUE;
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 import static org.springframework.http.HttpStatus.UNAUTHORIZED;
