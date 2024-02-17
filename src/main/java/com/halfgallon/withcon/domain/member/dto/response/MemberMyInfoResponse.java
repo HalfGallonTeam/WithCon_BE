f8e4b6594fc5e72226627943fd52837b7b7df8ec
Jinyoung Choi <68311264+cchoijjinyoung@users.noreply.github.com>
2024-02-17 10:23:21 +0900

    [refactor] 내 정보 조회 필드 추가 (#94)
    * feat: 내 정보 조회 필드 추가
    
    * feat: 내 정보 조회 필드 추가

--- src/main/java/com/halfgallon/withcon/domain/member/dto/response/MemberMyInfoResponse.java
+++ src/main/java/com/halfgallon/withcon/domain/member/dto/response/MemberMyInfoResponse.java
@@ -6,15 +6,19 @@ import lombok.Builder;
 
 @Builder
 public record MemberMyInfoResponse(
+    String username,
     String nickname,
     String phoneNumber,
+    String profileImage,
     LoginType loginType
 ) {
 
   public static MemberMyInfoResponse fromEntity(Member member) {
     return MemberMyInfoResponse.builder()
+        .username(member.getUsername())
         .nickname(member.getNickname())
         .phoneNumber(member.getPhoneNumber())
+        .profileImage(member.getProfileImage())
         .loginType(member.getLoginType())
         .build();
   }
