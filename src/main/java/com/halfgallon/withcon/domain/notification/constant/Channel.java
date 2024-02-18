7dda52a797b70bda1158628ccb382313e8be7f8a
sosa7753 <sosa7753@naver.com>
2024-02-18 21:16:09 +0900

    refactor 클래스 삭제

--- src/main/java/com/halfgallon/withcon/domain/notification/constant/Channel.java
+++
@@ -1,8 +0,0 @@
-package com.halfgallon.withcon.domain.notification.constant;
-
-public class Channel {
-
-  public static String makeChannel(Long performanceId, Long chatRoomId) {
-    return performanceId + "-" + chatRoomId;
-  }
-}
