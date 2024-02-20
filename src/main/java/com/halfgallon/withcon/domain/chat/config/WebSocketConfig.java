56e17f069269c360a39887b87c82565e0a20621f
kdmin0706 <gxxk76@gmail.com>
2024-02-20 15:25:11 +0900

    feat: withSockJS 설정 추가

--- src/main/java/com/halfgallon/withcon/domain/chat/config/WebSocketConfig.java
+++ src/main/java/com/halfgallon/withcon/domain/chat/config/WebSocketConfig.java
@@ -44,7 +44,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
   @Override
   public void registerStompEndpoints(StompEndpointRegistry registry) {
-    registry.addEndpoint("/ws")
-        .setAllowedOriginPatterns("*");
+    registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
   }
 }
