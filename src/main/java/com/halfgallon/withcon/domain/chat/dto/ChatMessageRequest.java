71d91d042d2aa263405f07517938562df3d252bb
kdmin0706 <gxxk76@gmail.com>
2024-02-23 21:18:59 +0900

    refactor: #116 채팅 메시지 엔드포인트 변경 및 설정 추가 1. 채팅방 정보를 request 시에 lastMsgId와 limit 파라미터를 받아와서 클라이언트에서 마지막으로 읽은 메시지와 해당 메시지 limit를 설정하여 조회할 내용의 사이즈를 설정한다. 2. 클라이언트에서 limit가 0이거나 null인 경우에는 서버 측에서 설정해놓은 page_size(10)으로 고정해서 사용한다.

--- src/main/java/com/halfgallon/withcon/domain/chat/dto/ChatMessageRequest.java
+++ src/main/java/com/halfgallon/withcon/domain/chat/dto/ChatMessageRequest.java
@@ -1,7 +1,16 @@
 package com.halfgallon.withcon.domain.chat.dto;
 
+import org.springframework.util.ObjectUtils;
+
 public record ChatMessageRequest(
-    Long lastMsgId
+    Long lastMsgId,
+    Integer limit
 ) {
+
+  public ChatMessageRequest {
+    if (ObjectUtils.isEmpty(limit) || limit == 0) {
+      limit = 0;
+    }
+  }
 
 }
