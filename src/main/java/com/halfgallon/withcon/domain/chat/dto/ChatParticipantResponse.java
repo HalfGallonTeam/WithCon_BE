364442611dbc9d10e682f79a40181bdcd57ef9d6
kdmin0706 <gxxk76@gmail.com>
2024-02-24 15:28:03 +0900

    feat: 나의 채팅방 조회 시에 태그 정보 `response` 추가

--- src/main/java/com/halfgallon/withcon/domain/chat/dto/ChatParticipantResponse.java
+++ src/main/java/com/halfgallon/withcon/domain/chat/dto/ChatParticipantResponse.java
@@ -1,13 +1,16 @@
 package com.halfgallon.withcon.domain.chat.dto;
 
 import com.halfgallon.withcon.domain.chat.entity.ChatParticipant;
+import com.halfgallon.withcon.domain.tag.entity.Tag;
+import java.util.List;
 import lombok.Builder;
 
 @Builder
 public record ChatParticipantResponse(
     Long memberId,
     Long chatRoomId,
-    String chatRoomName
+    String chatRoomName,
+    List<String> tags
 ) {
 
   public static ChatParticipantResponse fromEntity(ChatParticipant participant) {
@@ -15,6 +18,7 @@ public record ChatParticipantResponse(
         .memberId(participant.getMember().getId())
         .chatRoomId(participant.getChatRoom().getId())
         .chatRoomName(participant.getChatRoom().getName())
+        .tags(participant.getChatRoom().getTags().stream().map(Tag::getName).toList())
         .build();
   }
 
